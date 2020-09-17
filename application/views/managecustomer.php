0931363f64ba77d27bf30fc48640930bdce577e1
Nilraj <nilrajdhummad6d@gmail.com>
2020-09-17 13:57:22 +0530

    edit

--- application/views/managecustomer.php
+++ application/views/managecustomer.php
@@ -1,7 +1,5 @@
-<?php include('header.php'); ?>
+<?php include('header.php');?>
 <?php include('body.php'); ?>
-
-
 <div class="page-content">
                 <nav class="page-breadcrumb">
 					<ol class="breadcrumb">
@@ -19,6 +17,7 @@
                             <table id="dataTableExample" class="table">
                                 <thead>
                                     <tr>
+                                        <th>ID</th>
                                         <th>Customer ID</th>
                                         <th>Customer Name</th>
                                         <th>Email</th>
@@ -27,21 +26,45 @@
                                         <th>Mobile No.</th>
                                         <th>Password</th>
                                         <th>Status</th>
+                                        <th width="60">Edit</th>
+                                        <th width="100">Delete</th>
                                         <th>Function</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                 <?php if(!empty($new_customer)) { foreach($new_customer as $user){?>
                                 <tr>
-                                    <td><?php echo $user['customer_code'] ?></td>
-                                    <td><?php echo $user['customer_name'] ?></td>
-                                    <td><?php echo $user['email'] ?></td>
-                                    <td><?php echo $user['customer_type'] ?></td>
-                                    <td><?php echo $user['address'] ?></td>
-                                    <td><?php echo $user['mobile_no'] ?></td>
-                                    <td><?php echo $user['password'] ?></td>
-                                    <td><?php echo "status" ?></td>
-                                    <td><?php echo "function" ?></td>
+                                    <td><?php echo $user['sr_no'];  ?></td>
+                                    <td><?php echo $user['customer_code']; ?></td>
+                                    <td><?php echo $user['customer_name'] ;?></td>
+                                    <td><?php echo $user['email']; ?></td>
+                                    <td><?php echo $user['customer_type']; ?></td>
+                                    <td><?php echo $user['address']; ?></td>
+                                    <td><?php echo $user['mobile_no']; ?></td>
+                                    <td><?php echo $user['password']; ?></td>
+                                    <td><?php echo "status"; ?></td>
+                                    <td>
+                                    <a href="<?php echo base_url().'index.php/welcome/edit_customer/'.$user['sr_no']?>" class="btn btn-primary">
+                                        Edit</a>    
+                                    </td> 
+                                    <td>
+                                        <a href="<?php echo base_url().'index.php/welcome/delete_customer/'.$user['sr_no']?>" class="btn btn-danger">
+                                        delete</a>    
+                                    </td> 
+                                    <td><div class="btn-group flex-wrap">
+                                            <button type="button" class="mb-1 mt-1 mr-1 btn btn-default dropdown-toggle" data-toggle="dropdown">Action 
+                                                <span class="caret"></span>
+                                            </button>
+                                            <div class="dropdown-menu" role="menu"> 
+                                                <a class="dropdown-item " href="<?php echo base_url().'index.php/welcome/edit_customer'.$user['sr_no']?>">
+                                                    <i class=""></i> Edit
+                                                </a>
+                                                <a class="dropdown-item"  href="<?php echo base_url().'index.php/welcome/delete_customer'.$user['sr_no']?>">
+                                                    <i class=""></i> Delete
+                                                </a>
+                    						</div>
+                                      </div>
+                                    </td>
                                 </tr>
                                 <?php } } else { ?>
                                 <tr>
