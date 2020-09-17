efb4c7719a1f8db675262f6f1ef3882b25b059bb
Nilraj <nilrajdhummad6d@gmail.com>
2020-09-17 12:31:34 +0530

    edit

--- application/views/addproductgroup.php
+++ application/views/addproductgroup.php
@@ -8,56 +8,28 @@
                     <div class="card card-custom">
                         <div class="card-body">
                             <h6 class="card-title"><h1>Add New Executive:</h1></h6><br>
-                            <form method="post" action="<?php echo base_url().'index.php/welcome/addnewexecutive';?>">
+                            <form method="post" action="<?php echo base_url().'index.php/welcome/addproductgroup';?>">
                               <div class="row">
                                 <div class="col-xl-1">
                                 </div>
                                   <div class="col-lg-5 my-2 ">
                                     <div class="form-group">
-                                      <label class="control-label"><h4>Executive Code</h4></label>
-                                      <input type="text" name="executive_code" class="form-control border-dark" placeholder="Enter Executive" value= "<?php echo set_value('executive_code');?>">
-                                      <?php echo form_error('executive_code');?>
+                                      <label class="control-label"><h4>Product Group Code</h4></label>
+                                      <input type="text" name="product_group_code" class="form-control border-dark" placeholder="Enter Executive" value= "<?php echo set_value('product_group_code');?>">
+                                      <?php echo form_error('product_group_code');?>
                                     </div>
                                     <div class="form-group">
-                                      <label class="control-label"><h4>Executive Name</h4></label>
-                                      <input type="text" name="executive_name" class="form-control border-dark" placeholder="Enter Executive" value= "<?php echo set_value('executive_name');?>">
-                                      <?php echo form_error('executive_name');?>
-                                    </div>
-                                    <div class="form-group">
-                                      <label class="control-label"><h4>Executive Type</h4></label>
-                                      <input type="text" name="executive_type" class="form-control border-dark" placeholder="Enter Executive" value= "<?php echo set_value('executive_type');?>">
-                                      <?php echo form_error('executive_type');?>
-                                    </div>
-                                    <div class="form-group">
-                                      <label class="control-label"><h4>Email Address</h4></label>
-                                      <input type="text" name="email" class="form-control border-dark" placeholder="Enter Executive" value= "<?php echo set_value('email');?>">
-                                      <?php echo form_error('email');?>
-                                    </div>
-                                  </div><!-- Col -->
-                                  <div class="col-lg-5 my-2 ">
-                                    <div class="form-group">
-                                      <label class="control-label"><h4>Address</h4></label>
-                                      <textarea rows="4" name="address" type="text" class="form-control border-dark" placeholder="Enter Address" value= "<?php echo set_value('address');?>"></textarea>
-                                      <?php echo form_error('address');?>
-                                    </div>
-                                    <div class="form-group">
-                                      <label class="control-label"><h4>Mobile No.</h4></label>
-                                      <input type="text" name="mobile_no" class="form-control border-dark" placeholder="Enter Executive" value= "<?php echo set_value('mobile_no');?>">
-                                      <?php echo form_error('mobile_no');?>
-                                    </div>
-                                    <div class="form-group">
-                                      <label class="control-label"><h4>Password</h4></label>
-                                      <input type="text" name="password" class="form-control border-dark" placeholder="Enter Executive" value= "<?php echo set_value('password');?>">
-                                      <?php echo form_error('password');?>
-                                    </div>
-                                    
+                                      <label class="control-label"><h4>Product Group Name</h4></label>
+                                      <input type="text" name="product_group_name" class="form-control border-dark" placeholder="Enter Executive" value= "<?php echo set_value('product_group_name');?>">
+                                      <?php echo form_error('product_group_name');?>
+                                    </div>                                    
                                   </div><!-- Col -->
                               </div><!-- Row -->
                               <div class="separator separator-dashed my-10">
                               </div>                
                             <div class="form-group align-center">
                             <button  class="btn btn-primary "><h4>Submit</h4></button>    
-                            <a href="<?php echo base_url().'index.php/welcome/addnewexecutive';?>" class="btn-secondary btn"><h4>Reset</h4></a>
+                            <a href="<?php echo base_url().'index.php/welcome/addproductgroup';?>" class="btn-secondary btn"><h4>Reset</h4></a>
                             </div>
                             </form>
                         </div>
--- application/views/manage_product_group.php
+++ application/views/manage_product_group.php
@@ -6,7 +6,7 @@
                 <nav class="page-breadcrumb">
 					<ol class="breadcrumb">
 						<li class="breadcrumb-item"><a href="#">Product</a></li>
-						<li class="breadcrumb-item active" aria-current="page">Manage Product Model</li>
+						<li class="breadcrumb-item active" aria-current="page">Manage Product Group</li>
 					</ol>
 				</nav>
 
@@ -14,26 +14,23 @@
 					<div class="col-md-12 grid-margin stretch-card">
                         <div class="card">
                         <div class="card-body">
-                            <h6 class="card-title">Manage Product Model</h6>
+                            <h6 class="card-title">Manage Product Group</h6>
                             <div class="table-responsive">
                             <table id="dataTableExample" class="table">
                                 <thead>
                                     <tr>
-                                        <th>Product Model Code</th>
-                                        <th>Product Model Name</th>
-                                        <th>Select Product</th>
-                                        
+                                        <th>Product Group Code</th>
+                                        <th>Product Group Name</th>
                                         <th>Status</th>
                                         <th>Function</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                <?php if(!empty($new_product_Model)) { foreach($new_product_model as $user){?>
+                                <?php if(!empty($new_product_group)) { foreach($new_product_group as $user){?>
                                 <tr>
-                                    <td><?php echo $user['product_model_code'] ?></td>
-                                    <td><?php echo $user['product_model_name'] ?></td>
-                                    <td><?php echo $user['select_product'] ?></td>
-                                   <td><?php echo "status"?></td>
+                                    <td><?php echo $user['product_group_code'] ?></td>
+                                    <td><?php echo $user['product_group_name'] ?></td>
+                                    <td><?php echo "status"?></td>
                                     <td><?php echo "function" ?></td>
                                 </tr>
                                 <?php } } else { ?>
